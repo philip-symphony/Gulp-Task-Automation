63d4ad19f1a0d73f875991f82a45b2dddfdf106a
philip-symphony <philipb@symphonycommerce.com>
2015-08-18 11:16:08 -0700

    Create repo for Symphony team

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# Gulp-Task-Automation
-Lint, compile, autoprefix, sourcemaps, browser sync + proxy
+## Gulp Task Automation
+
+### Global dependencies
+
+These need to be installed one time to be available globally on your system.
+
+- node - [download installer](http://nodejs.org/)
+- gulp - `npm install -g gulp`
+- sass - `sudo gem install sass`
+
+### Initial setup
+(Only one team member needs to do this setup)
+
+**1. Create file structure**
+
+For this example, the file structure looks like this:
+```
+/
+|
+|-- gulpfile.js
+|-- package.json
+|
+|-- /app
+|   |-- /css
+|   |-- /images
+|   `-- /js
+|
+`-- /src
+    |-- /images
+    |-- /js
+    `-- /scss
+        `-- main.scss
+```
+
+**2. Create a `package.json` file in the root directory for this project.**
+
+`$ npm init`
+
+**3. Install dependencies. If dependencies are added for new tasks remember to use `--save-dev` flag!**
+
+`$ npm install --save-dev gulp browser-sync gulp-autoprefixer gulp-cached gulp-concat gulp-filter gulp-imagemin gulp-jshint gulp-notify gulp-plumber gulp-rename gulp-sass gulp-sourcemaps gulp-uglify serve-static`
+
+**Do NOT use sudo!**
+
+Here is an example of what the DevDependencies section will look like in your package.json file:
+
+```
+{
+  "devDependencies": {
+    "browser-sync": "^2.8.2",
+    "gulp": "^3.8.10",
+    "gulp-autoprefixer": "^2.0.0",
+    "gulp-cached": "^1.0.4",
+    "gulp-concat": "^2.4.2",
+    "gulp-filter": "^2.0.0",
+    "gulp-imagemin": "^2.0.0",
+    "gulp-jshint": "^1.9.0",
+    "gulp-notify": "^2.1.0",
+    "gulp-plumber": "^0.6.6",
+    "gulp-rename": "^1.2.0",
+    "gulp-sass": "^2.0.1",
+    "gulp-sourcemaps": "^1.2.8",
+    "gulp-uglify": "^1.0.2",
+    "serve-static": "^1.10.0"
+  }
+}
+```
+
+**4. Set file paths and browser support in `gulpfile.js`**
+
+All paths are relative to `gulpfile.js`. Autoprefixer browser support must be precise or the task will fail. Refer to the [documentation](https://github.com/ai/browserslist) when adding browsers.
+
+**5. Update `.gitignore`**
+
+The repository does not need to include the `node_modules` directory. The dependencies are listed in `package.json`.
+
+### Bring a team member into the project
+**1. `$ git clone`**
+
+The repository should include:
+- `gulpfile.js`
+- `package.json`
+
+**2. `$ npm install`**
+
+Now everyone on the team has the same version.
+
+### Gulp Basics
+To run the 'default' task one time
+
+`$ gulp`
+
+To start gulp and continue watching
+
+`$ gulp watch`
+
+To start gulp and continue watching with BrowserSync
+
+`$ gulp bsync`
---
+++ gulpfile.js
@@ -0,0 +1,191 @@
+//------------------------------------------------------------------------------
+// Dependencies
+//------------------------------------------------------------------------------
+var gulp = require('gulp');
+
+var autoprefixer    = require('gulp-autoprefixer'),
+    browsersync     = require('browser-sync'),
+    cache           = require('gulp-cached'),
+    concat          = require('gulp-concat'),
+    filter          = require('gulp-filter'),
+    imagemin        = require('gulp-imagemin'),
+    jshint          = require('gulp-jshint'),
+    notify          = require('gulp-notify'),
+    plumber         = require('gulp-plumber'),
+    rename          = require('gulp-rename'),
+    sass            = require('gulp-sass'),
+    servestatic     = require('serve-static'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    uglify          = require('gulp-uglify');
+
+// -----------------------------------------------------------------------------
+// Configuration
+// -----------------------------------------------------------------------------
+
+// Development URL
+var devUrl = 'https://demo.example-site.com';
+
+// Local file paths
+var src     = 'src',
+    dest    = 'app';
+
+// Supported browsers
+var browserSupport = {
+    browsers: [
+        'last 2 version',
+        'safari 5',
+        'ie 8',
+        'ios 6',
+        'android 4'
+    ]
+};
+
+//------------------------------------------------------------------------------
+// BrowserSync task
+//------------------------------------------------------------------------------
+gulp.task('browsersync', function() {
+    browsersync.init({
+        notify: false,
+        // Proxy dev site
+        proxy: devUrl,
+        https: true,
+        // Inject local files
+        rewriteRules: [
+            {
+                match: new RegExp('https://s3.amazonaws.com/sneakpeeq-sites/example-site/styles/main.css'),
+                fn: function() {
+                    return 'main.css'
+                }
+            }
+        ],
+        files: 'app/css/*.css',
+        middleware: servestatic('./app/css')
+    });
+});
+
+//------------------------------------------------------------------------------
+// Error handling
+//------------------------------------------------------------------------------
+function onError(error) {
+    var errorTitle = '[' + error.plugin + ']',
+        errorString = error.message;
+
+    if (error.lineNumber) {
+        errorString += ' on line ' + error.lineNumber;
+    }
+    if (error.fileName) {
+        errorString += ' in ' + error.fileName;
+    }
+
+    notify.onError({
+        title:    errorTitle,
+        message:  errorString,
+        sound:    "Beep"
+    })(error);
+    this.emit('end');
+}
+
+//------------------------------------------------------------------------------
+// Styles task
+//------------------------------------------------------------------------------
+gulp.task('styles', function() {
+    return gulp.src(src + '/scss/**/*.scss')
+    .pipe(plumber({errorHandler: onError}))
+
+    // Compile
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write())
+
+    // Autoprefixer
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(autoprefixer( browserSupport ))
+    .pipe(sourcemaps.write())
+
+    // Write css
+    .pipe(gulp.dest(dest + '/css'))
+
+    // CSS Injection
+    .pipe(filter('**/*.css'))
+    .pipe(browsersync.reload({ stream: true }))
+
+    // Notification
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+//------------------------------------------------------------------------------
+// Scripts task
+//------------------------------------------------------------------------------
+gulp.task('scripts', function() {
+    return gulp.src(src + '/js/**/*.js')
+
+    // Cache
+    .pipe(cache('scripts'))
+
+    // JSHint & compile
+    // TO DO: fix error reporting
+    .pipe(jshint())
+    .pipe(jshint.reporter('fail'))
+    .on('error', notify.onError ({
+        title: 'Error!',
+        message: 'Scripts task failed'
+    }))
+
+    // Compile
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(dest + '/js'))
+
+    // Minify
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest(dest + '/js'))
+
+    // Notification
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+//------------------------------------------------------------------------------
+// Images task
+//------------------------------------------------------------------------------
+gulp.task('images', function() {
+    return gulp.src(src + '/images/**/*')
+    .pipe(plumber({errorHandler: onError}))
+
+    // Compress & cache
+    .pipe(cache('images'))
+
+    .pipe(imagemin({
+        optimizationLevel: 3,
+        progressive: true,
+        interlaced: true
+    }))
+    .pipe(gulp.dest(dest + '/images'))
+
+    // Notification
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+//------------------------------------------------------------------------------
+// Default task
+//------------------------------------------------------------------------------
+gulp.task('default', ['styles', 'scripts', 'images']);
+
+//------------------------------------------------------------------------------
+// Watch task
+//------------------------------------------------------------------------------
+gulp.task('watch', function() {
+    gulp.watch(src + '/scss/**/*.scss', ['styles']);
+    gulp.watch(src + '/js/**/*.js', ['scripts']);
+    gulp.watch(src + '/images/**/*', ['images']);
+});
+
+//------------------------------------------------------------------------------
+// Browser Sync task [bsync]
+//------------------------------------------------------------------------------
+gulp.task('bsync', ['browsersync'], function() {
+    gulp.watch(src + '/scss/**/*.scss', ['styles']);
+    gulp.watch(src + '/js/**/*.js', ['scripts', browsersync.reload]);
+    gulp.watch(src + '/images/**/*', ['images']);
+});
